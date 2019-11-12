38892099be086293c3e07221a26eb8ebeead3ef9
vianney <viannnne@gmail.com>
2019-11-12 04:41:48 -0600

    update

--- app/src/main/java/com/suramexico/gcd/Main2Activity.java
+++ app/src/main/java/com/suramexico/gcd/Main2Activity.java
@@ -2,26 +2,35 @@ package com.suramexico.gcd;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.suramexico.gcd.utils.Constants;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main2Activity extends AppCompatActivity {
     private TextView tvNombre;
@@ -31,10 +40,18 @@ public class Main2Activity extends AppCompatActivity {
     private ImageButton ibCamera;
     private ImageButton ibWeb;
 
-    private final String[] PERMISSIONS = {
+    private final String[] PERMISSIONS_MEDIA = {
             Manifest.permission.CAMERA
     };
-    private final int REQUEST_CAMERA =101;
+    private final String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+    private Uri photoURI;
+    private final int REQUEST_CAMERA = 101;
+    private final int REQUEST_STORAGE = 102;
+    private final int REQUEST_TAKE_PHOTO = 103;
 
 
     @Override
@@ -43,7 +60,8 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         initView();
         loadValues();
-        requestPermissions(Main2Activity.this);
+        requestPermissions(Main2Activity.this, PERMISSIONS_MEDIA, REQUEST_CAMERA);
+        requestPermissions(Main2Activity.this, PERMISSIONS_STORAGE, REQUEST_STORAGE);
     }
 
     private void initView(){
@@ -60,6 +78,7 @@ public class Main2Activity extends AppCompatActivity {
         tvApellidoMaterno.setText(getCustomExtras(Constants.APELLIDO_MATERNO));
         tvApellidoPaterno.setText(getCustomExtras(Constants.APELLIDO_PATERNO));
         ibWeb.setOnClickListener(new SendWebListener());
+        ibCamera.setOnClickListener(new TomarFotoListener());
 
     }
 
@@ -83,23 +102,21 @@ public class Main2Activity extends AppCompatActivity {
             if(!getIntent().getExtras().getString(clave).isEmpty()){
                 return getIntent().getExtras().getString(clave);
             }else{
-                return "No disponible";
+                return getResources().getString(R.string.mensaje_texto_no_disponible);
             }
         }else{
-            return "No disponible";
+            return getResources().getString(R.string.mensaje_texto_no_disponible);
         }
 
     }
 
 
-    public void requestPermissions(Activity context){
-        if(ActivityCompat.checkSelfPermission(context , Manifest.permission.CAMERA) !=
-                PackageManager.PERMISSION_GRANTED ){
-            ActivityCompat.requestPermissions(context, PERMISSIONS, REQUEST_CAMERA);
-            Toast.makeText(Main2Activity.this, "Es necesario conseder el permiso",
-                    Toast.LENGTH_LONG).show();
+    public void requestPermissions(Activity context, String[] permissions, int requestCode){
+        if(ActivityCompat.checkSelfPermission(context , permissions[0]) != PackageManager.PERMISSION_GRANTED ){
+            ActivityCompat.requestPermissions(context, permissions, requestCode);
         }else{
-            Toast.makeText(Main2Activity.this, "Permiso consedido", Toast.LENGTH_LONG).show();
+            Toast.makeText(Main2Activity.this,
+                    getResources().getString(R.string.mensaje_permiso_concedido), Toast.LENGTH_LONG).show();
         }
     }
 
@@ -107,9 +124,95 @@ public class Main2Activity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         if(requestCode == REQUEST_CAMERA){
-            requestPermissions(Main2Activity.this);
+            if((grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+                Toast.makeText(Main2Activity.this,
+                        getResources().getString(R.string.mensaje_permiso_concedido), Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(Main2Activity.this,
+                        getResources().getString(R.string.mensaje_permiso_necesario), Toast.LENGTH_LONG).show();
+                requestPermissions(Main2Activity.this, PERMISSIONS_MEDIA, REQUEST_CAMERA);
+            }
 
+        }else if(requestCode == REQUEST_STORAGE){
+            if((grantResults.length > 0 && grantResults[1] == PackageManager.PERMISSION_GRANTED)){
+                Toast.makeText(Main2Activity.this,
+                        getResources().getString(R.string.mensaje_permiso_concedido), Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(Main2Activity.this,
+                        getResources().getString(R.string.mensaje_permiso_necesario), Toast.LENGTH_LONG).show();
+                requestPermissions(Main2Activity.this, PERMISSIONS_STORAGE, REQUEST_STORAGE);
+            }
         }
 
+    }
+
+
+
+    private class TomarFotoListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            if(ActivityCompat.checkSelfPermission(Main2Activity.this , Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ){
+                requestPermissions(Main2Activity.this, PERMISSIONS_MEDIA, REQUEST_CAMERA);
+            }else{
+                TomarFoto();
+            }
+        }
+    }
+
+
+    private void TomarFoto(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = getFilePhoto();
+            if (photoFile != null) {
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE, "Avatar");
+                values.put(MediaStore.Images.Media.DESCRIPTION, "Foto_" + System.currentTimeMillis());
+                photoURI = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+
+    private File getFilePhoto(){
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            Toast.makeText(Main2Activity.this, "" + ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        return photoFile;
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        return image;
+    }
+
+    private Bitmap getBitmap(Uri photoURI){
+        Bitmap bitmap = null;
+        try {
+        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), photoURI);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            ivAvatar.setImageBitmap(getBitmap(photoURI));
+        }
     }
 }
