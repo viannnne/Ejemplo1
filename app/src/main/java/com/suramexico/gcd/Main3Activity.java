a2659fb0cf4ef48b7fe66b1d31cea19e328097ab
vianney <viannnne@gmail.com>
2019-11-12 05:31:40 -0600

    update

--- app/src/main/java/com/suramexico/gcd/Main3Activity.java
+++ app/src/main/java/com/suramexico/gcd/Main3Activity.java
@@ -8,6 +8,9 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class Main3Activity extends AppCompatActivity {
@@ -27,6 +30,26 @@ public class Main3Activity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+      
+        switch (item.getItemId()) {
+            case R.id.action_about_of:;
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
